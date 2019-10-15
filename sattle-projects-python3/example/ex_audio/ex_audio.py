64a71378d06cabe0d8ef3b1ad7249b08831de6b7
junghyunseo <junghyunseo0105@gmail.com>
2019-10-16 07:30:05 +0900

    Update ex_audio.py

--- sattle-projects-python3/example/ex_audio/ex_audio.py
+++ sattle-projects-python3/example/ex_audio/ex_audio.py
@@ -1,5 +1,21 @@
 from pygame import mixer # Load the required library
+from mutagen.mp3 import MP3
+import time
 
 mixer.init()
-mixer.music.load('/home/pi/ai-makers-kit/data/sample_sound.wav')
+path = '/home/pi/sattle-projects-python3/audioFile/나중에넌아주크게될텐데.mp3'
+mixer.music.load(path)
+audio = MP3(path)
+length = audio.info.length
+print(length)
 mixer.music.play()
+
+time.sleep(length)
+
+mixer.init()
+mixer.music.load('/home/pi/sattle-projects-python3/audioFile/코소리.mp3')
+mixer.music.play()
+
+while True:
+    pass
+    
